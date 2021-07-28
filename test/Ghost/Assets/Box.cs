e8f8dc79a7189f7a2369c223815f8a7ac27cd5e4
0zw <g1927020@st.t-kougei.ac.jp>
2021-07-28 12:12:33 +0900

    とりつきの共有

---
+++ test/Ghost/Assets/Box.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    [SerializeField] private float radius=0.0f;
+    [SerializeField] private float angle=0.0f;
+    [SerializeField] private float y = 0.0f;
+    [SerializeField] private float speed = 100.0f;
+    private float x = 0.0f;
+    private float z = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKey("right"))
+       {
+            angle += Time.deltaTime*speed;
+       }
+       else if(Input.GetKey("left"))
+       {
+            angle -= Time.deltaTime*speed;
+            
+       }
+       if(angle>360.0f)
+       {
+            angle -= 360.0f;
+       }
+       else if(angle<0.0f)
+       {
+            angle += 360.0f;
+       }
+            x = (radius * Mathf.Sin((angle) * Mathf.Deg2Rad));
+            z = (radius * Mathf.Cos((angle) * Mathf.Deg2Rad));
+        this.transform.localPosition = new Vector3(x, y, z);
+        this.transform.localRotation = Quaternion.Euler(0.0f,angle,0.0f);
+    }
+}
---
+++ test/Ghost/Assets/EnemySearch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySearch : MonoBehaviour
+{
+    public List<GameObject> searchObject = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Monkey" || other.tag == "Box")
+        {
+
+            if (!searchObject.Contains(other.gameObject))
+            {
+                searchObject.Add(other.gameObject);
+            }
+
+
+        }
+        else
+        {
+            if (searchObject.Contains(other.gameObject))
+            {
+                searchObject.Remove(other.gameObject);
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy" || other.tag == "Box")
+        {
+
+            if (searchObject.Contains(other.gameObject))
+            {
+                searchObject.Remove(other.gameObject);
+            }
+        }
+    }
+}
